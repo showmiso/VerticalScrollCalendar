cc1f440c5f5d1ff1c64f6b323bfdc1f814777f55
Miso Park <showmiso@gmail.com>
2020-11-13 09:28:36 +0900

    Config NavHostFragment and BottomNavView

--- app/src/main/java/com/showmiso/verticalscrollcalendar/today/TodayActivity.kt
+++ app/src/main/java/com/showmiso/verticalscrollcalendar/today/TodayActivity.kt
@@ -2,11 +2,17 @@ package com.showmiso.verticalscrollcalendar.today
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.showmiso.verticalscrollcalendar.R
+import kotlinx.android.synthetic.main.activity_today.*
 
 class TodayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_today)
+
+        val navController = findNavController(R.id.fragment)
+        nav_bottom_view.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/showmiso/verticalscrollcalendar/today/TodayFragment.kt
@@ -0,0 +1,19 @@
+package com.showmiso.verticalscrollcalendar.today
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.showmiso.verticalscrollcalendar.R
+
+class TodayFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_today, container, false)
+    }
+}
